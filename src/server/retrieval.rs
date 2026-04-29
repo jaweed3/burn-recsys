9859ef197bfd55ac0bebc027d46bcf32e5d7af6f
jaweed3 <wedjaw22@gmail.com>
2026-04-29 08:05:22 +0700

    feat: working on #11, implementing Point and Retrievable

--- src/server/retrieval.rs
+++ src/server/retrieval.rs
@@ -1,5 +1,46 @@
-use rand::prelude::*;
+use instant_distance::{Builder, Hnsw, MapItem};
+use serde::{Deserialize, Serialize};
+use rand::{prelude::*, seq::index};
 use std::collections::HashSet;
+
+#[derive(Clone, Serialize, Deserialize)]
+struct Point {
+    vector: Vec<f32>,
+    item_id: u32,
+}
+
+impl instant_distance::Point for Point {
+    fn distance(&self, other: &Self) -> f32 {
+        let dist: f32 = self.vector.iter()
+            .zip(&other.vector)
+            .map(|(a, b)| (a - b).powi(2))
+            .sum();
+        dist.sqrt()
+    }
+}
+
+pub struct VectorRetriever {
+    index: Hnsw<Point>,
+}
+
+impl VectorRetriever {
+    pub fn new(embeddings: Vec<Vec<f32>>) -> Self {
+        let points: Vec<Point> = embeddings.into_iter().enumerate()
+            .map(|(i, v)| Point {vector: v, item_id: i as u32})
+            .collect();
+
+        let index = Builder::default().build(points, vec![(); points.len()]);
+
+        Self { index }
+    }
+}
+
+impl CandidateGenerator for VectorRetriever {
+    fn generate(&self, user_id: u32, limit: usize, exclude: &HashSet<u32>) -> Vec<u32> {
+        // TODO: take user embedding from model to search nearest relevant item.
+        vec![]
+    }
+}
 
 pub trait CandidateGenerator: Send + Sync {
     fn generate(&self, user_id: u32, limit: usize, exclude: &HashSet<u32>) -> Vec<u32>;
